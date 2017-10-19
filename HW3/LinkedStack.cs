68585420cfe9e3fcb9120843e900d0ada2f31f32
LeoThalman <lthalman15@wou.edu>
2017-10-18 22:29:01 -0700

    Added LinkedStack, Stack implementation of IStackADT

---
+++ HW3/LinkedStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    /**
+     * A singly linked stack.
+     */ 
+    class LinkedStack : IStackADT
+    {
+        private Node top;
+
+        public LinkedStack()
+        {
+            top = null; //empty stack
+        }
+
+        public Object Push(Object newItem)
+        {
+            if (newItem == null)
+            {
+            }
+            //if not null add to list, else return null
+            else
+            {
+                Node newNode = new Node(newItem, top);
+                top = newNode;
+            }
+            return newItem;
+        }
+
+        //if Stack is empty return null, else pop top item on stack
+        public Object Pop()
+        {
+            Object rItem = null;
+            if(IsEmpty() )
+            {
+            }
+            else
+            {
+                Object rItem = top.data;
+                top = top.next;
+            }
+            return rItem;
+        }
+
+        //if stack empty return null, else return top item
+        public Object Peek()
+        {
+            Object rItem = null;            
+            if (IsEmpty() )
+            {
+            }
+            else
+            {
+                rItem = top.data();
+            }
+            return rItem;
+        }
+
+        public Boolean IsEmpty()
+        {
+            return top == null;
+        }
+
+        public void Clear()
+        {
+            top = null;
+        }
+    }
+}
